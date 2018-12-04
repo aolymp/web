d169ae8aef7bd7df86488a77b49567d9fcbd6e1d
Andrey K <pipo@uolimp.ru>
2018-12-04 18:09:45 +0500

    lern django-2

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo rm -f /etc/gunicorn.d/*
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn
+sudo ln -s /home/box/web/etc/django.conf /etc/gunicorn.d/django
+
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start

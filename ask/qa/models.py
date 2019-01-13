18eae4e6bb3eff20079f894ac029e56ad6338ca7
Andrey K <pipo@uolimp.ru>
2019-01-13 10:24:54 +0500

    set models-1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=254)
+	text = models.TextField()
+	added_at = models.DateTimeField(default=timezone.now)
+	rating = models.IntegerField(default=0)
+	author = ForeignKey(User, on_delete=models.CASCADE)
+	likes = models.ManyToManyField(User, related_name='likes_set')
+	def __unicode__(self):
+		return self.title
+
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.objects.order_by('added_at').reverse()
+	def popular(self):
+		return self.objects.order_by('rating').reverse()
+
+class Answer(models.Model):
+	title = models.CharField(max_length=254)
+	text = models.TextField()
+	added_at = models.DateTimeField(default=timezone.now)
+	question = models.ForeignKey(Question)
+	author = ForeignKey('auth.User', on_delete=models.CASCADE)
--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ sudo /etc/init.d/nginx restart
 sudo rm -f /etc/gunicorn.d/*
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn
 sudo ln -s /home/box/web/etc/django.conf /etc/gunicorn.d/django
-
 sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql start
+echo "create database ..."
+mysql -uroot -e 'CREATE DATABASE `ural` CHARACTER SET utf8;'
+mysql -uroot -e 'CREATE USER "portal"@"%" IDENTIFIED BY "m5portal";'
+mysql -uroot -e 'GRANT ALL PRIVILEGES ON ural.* TO "portal"@"%";'
+mysql -uroot -e 'FLUSH PRIVILEGES;'
